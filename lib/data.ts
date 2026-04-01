9f4c02dc0fc0353f2461adb42c02893731f850bd
PhanNam4253 <pvhn.pvhn@gmail.com>
2026-04-01 13:08:06 +0700

    refactor: fetch data through db instead of api

---
+++ lib/data.ts
@@ -0,0 +1,44 @@
+import prisma from "./prisma";
+
+export async function getCountries() {
+  return prisma.country.findMany({ orderBy: { name: "asc" } });
+}
+
+export async function getCities(countrySlug: string) {
+  const country = await prisma.country.findUnique({
+    where: {
+      slug: countrySlug,
+    },
+  });
+  if (!country) return [];
+  return prisma.city.findMany({
+    where: { countryId: country.id },
+    orderBy: {
+      name: "asc",
+    },
+  });
+}
+
+export async function getCollections(citySlug: string) {
+  const city = await prisma.city.findFirst({
+    where: {
+      slug: citySlug,
+    },
+  });
+  if (!city) return [];
+  return prisma.collection.findMany({
+    where: { cityId: city.id },
+    orderBy: {
+      name: "asc",
+    },
+  });
+}
+
+export async function getPhotos(collectionId: number) {
+  return prisma.photo.findMany({
+    where: { collectionId },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
